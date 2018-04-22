0694bdf01200abb0cdde545c55545bdab8813b52
Kr4ken-9 <kr4kenkt@gmail.com>
2018-04-21 22:43:15 -0500

    Join Servers via Commandline

--- raid.py
+++ raid.py
@@ -3,8 +3,11 @@ import asyncio
 import argparse
 
 client = discord.Client()
+
 parser = argparse.ArgumentParser()
 parser.add_argument('token', type=str, help='Token of the user to login as')
+parser.add_argument('-i', '--invite', nargs='?', default='default', help='Join a server using this invite')
+args = parser.parse_args()
 
 @client.event
 async def on_ready():
@@ -13,6 +16,8 @@ async def on_ready():
     print(client.user.id)
     print('------')
 
+    if args.invite != 'default':
+        await client.accept_invite(args.invite)
 
 @client.event
 async def on_message(message):
@@ -45,4 +50,4 @@ async def raid(message):
         await large(message.channel)
         await asyncio.sleep(2)
 
-client.run(parser.parse_args().token, bot=False)
+client.run(args.token, bot=False)
